04e8e02a649a3bfb70d6b73500332a0d5389a464
lse30 <38194693+lse30@users.noreply.github.com>
2019-11-08 17:43:14 +1300

    added a function to place numbers in the map

--- main.js
+++ main.js
@@ -12,10 +12,70 @@ function mapInit(size) {
     return map;
 }
 
+
+
+function addNums(map) {
+    //adds numbers to the board based on surrounding mines
+    let size = map.length;
+    let count;
+
+    for (let x = 0; x < size; x++) {
+        for (let y = 0; y < size; y++) {
+            count = 0;
+            if (map[x][y] !== 'M') {
+
+
+                if (y > 0 && map[x][y - 1] === 'M') {
+                    count++;
+                }
+                if (y < size - 1 && map[x][y + 1] === 'M') {
+                    count++;
+                }
+                if (x > 0 && map[x - 1][y] === 'M') {
+                    count++;
+                }
+                if (x < size - 1 && map[x + 1][y] === 'M') {
+                    count++;
+                }
+
+
+                if (y > 0 && x > 0 && map[x - 1][y - 1] === 'M') {
+                    count++;
+                }
+                if (y < size - 1 && x > 0  && map[x - 1][y + 1] === 'M') {
+                    count++;
+                }
+                if (y > 0 && x < size - 1 &&  map[x + 1][y - 1] === 'M') {
+                    count++;
+                }
+                if (y < size - 1 && x < size - 1 && map[x + 1][y + 1] === 'M') {
+                    count++;
+                }
+
+
+
+
+
+
+
+                if (count !== 0) {
+                    map[x][y] = count.toString();
+                }
+
+            }
+
+
+        }
+    }
+    return map
+}
+
 function MineMap(size, mines) {
+    //builds minesweeper map based on the size and number of mines
     let mineLocations = new Set([]);
     let trueSize = size ** 2;
 
+    //randomly assign positions to mines
     while (mineLocations.size < mines) {
         mineLocations.add(Math.floor((Math.random() * trueSize)))
     }
@@ -30,13 +90,16 @@ function MineMap(size, mines) {
         let y = mineLocations[i] % size;
         map[x][y] = 'M';
     }
-    console.log(map)
+    //console.log(map)
 
+
+    let finalmap = addNums(map);
+    console.log(finalmap)
 }
 
 function main()
 {
-    MineMap(10, 30);
+    MineMap(15, 40);
 }
 
 main();
